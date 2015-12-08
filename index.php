38765820bfe0e0cd9b0da47845ead743d8e2d93b
tectygroup <qycity@outlook.com>
2015-12-09 01:12:27 +0800

    Introduced a new way to contribute the content.

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-//引用原来的主页
-include 'include/index.php';
+	//只有小分类的地方需要引用这样的类
+	include 'include/Functions.php';
+	
+	//都可以引用的部分
+	include 'include/conf.php';
+	include 'include/header.php';
+	//下面的部分可以填写内容
+	
+	pic("img/example.jpg", "#fff", "这里应该是内容", "20px", "20px");
+?>
 
+
+
+
+
+
+<?php
+	include 'include/footer.php';
 ?>
